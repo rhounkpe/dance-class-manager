85e49d543fea1399d058b98023e7aaa8c0413d79
Rufin Hounkpe <rhounkpe@gmail.com>
2020-10-02 15:34:10 +0200

    User interface in command line

--- src/com/capgemini/Main.java
+++ src/com/capgemini/Main.java
@@ -2,29 +2,81 @@ package com.capgemini;
 
 import com.capgemini.model.Agenda;
 import com.capgemini.model.DanceClass;
-import com.capgemini.model.Room;
 
-import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
+    private static Agenda agenda;
 
-    public static void main(String[] args) {
-        Agenda agenda;
-
-        // Scanner sc = new Scanner(System.in);
-        // String name = sc.nextLine();
-
+    public static void main(String[] args) throws Throwable {
         agenda = new Agenda();
+        Scanner sc = new Scanner(System.in);
 
-        DanceClass tango = new DanceClass("Cours de Tango", Room.BERLIN, LocalDate.now());
-        agenda.addDanceClass("Cours de Tango", "BERLIN", "10 02 2020");
-        agenda.addDanceClass("Cours de Salsa", "Tokyo", "10 03 2020");
-        agenda.getEventList();
+        System.out.println("\n\nWelcome to our Agenda!\n");
 
+        String option = "";
 
-        for (DanceClass ds : agenda.getEventList()) {
-            System.out.println(ds);
+        while (!option.equalsIgnoreCase("x")) {
+            option = displayMenu(sc);
+        }
+
+        System.out.println("\nExiting System...\n");
+    }
+
+    private static String displayMenu(Scanner sc) throws Throwable {
+        System.out.println("Please select an option: ");
+        System.out.println("\t1. Enter a Dance Class");
+        System.out.println("\t2. Display Dance Classes");
+        System.out.println("\tX. Exit System");
+
+        String option = sc.next();
+
+        switch (option) {
+            case "1": encodeDanceClass(sc);
+                return option;
+            case "2": displayDanceClasses();
+                return option;
+            default:
+                System.out.println("Invalid option, please re-enter your choice!");
+                return option;
         }
     }
+
+    /**
+     * Encode new Dance Class
+     * @param sc
+     */
+    private static void encodeDanceClass(Scanner sc) {
+        System.out.println("Please enter Dance Class info");
+        sc.nextLine();
+        System.out.println("Dance Class name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Where?: ");
+        String roomKey = sc.nextLine();
+
+        System.out.println("When? (MM dd yyyy): ");
+        String date = sc.nextLine();
+
+        try {
+            agenda.addDanceClass(name, roomKey, date);
+        } catch (Throwable t) {
+            System.out.println("Error! " + t.getMessage());
+            return;
+        }
+
+    }
+
+    private static void displayDanceClasses() throws Throwable {
+        System.out.println("All Dance Classes");
+
+        for (DanceClass danceClass : agenda.getEventList()) {
+            System.out.println(danceClass);
+        }
+
+        System.out.println("\nPress any key to continue...");
+        System.in.read();
+        System.out.println("\n");
+    }
+
 }
